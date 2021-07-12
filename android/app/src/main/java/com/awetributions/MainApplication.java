e4e104eabe3f77e2b8dafb8ae569fa7f61012224
shensven <shensven2016@gmail.com>
2021-07-12 21:07:28 +0800

    fix: Complete the package react-native-vector-icons installation

--- android/app/src/main/java/com/awetributions/MainApplication.java
+++ android/app/src/main/java/com/awetributions/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -12,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.zoontek.rnbootsplash.RNBootSplashPackage;
 
 public class MainApplication extends Application implements ReactApplication {
@@ -29,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          new VectorIconsPackage();
           return packages;
         }
 
