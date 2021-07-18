299e0058a69730fc39df2bdce6978e5d5cd170b3
shensven <shensven2016@gmail.com>
2021-07-19 01:16:10 +0800

    feat: UI refine

---
+++ src/components/RadioBtnBlock.tsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import {View, Text, StyleSheet, StyleProp, ViewStyle} from 'react-native';
+import {
+    RadioButton,
+    TouchableRipple,
+    useTheme as usePaperTheme,
+} from 'react-native-paper';
+
+interface Props {
+    label: string;
+    checked: boolean;
+    onPress: () => void;
+    style?: StyleProp<ViewStyle>;
+}
+
+const RadioBtnBlock: React.FC<Props> = props => {
+    const {label, checked, onPress, style} = props;
+
+    const {colors: PaperColor} = usePaperTheme();
+
+    return (
+        <View
+            style={
+                checked
+                    ? [
+                          styles.item,
+                          {backgroundColor: PaperColor.RadioBtnBlockBackground},
+                          style,
+                      ]
+                    : [
+                          styles.item,
+                          {
+                              backgroundColor:
+                                  PaperColor.RadioBtnBlockBackgroundUnchecked,
+                          },
+                          style,
+                      ]
+            }>
+            <TouchableRipple rippleColor="rgba(0, 0, 0, 0.1)" onPress={onPress}>
+                <View style={styles.item_root}>
+                    <RadioButton
+                        status={checked ? 'checked' : 'unchecked'}
+                        value=""
+                        color={PaperColor.RadioBtnBlockText}
+                        uncheckedColor={PaperColor.RadioBtnBlockTextUnchecked}
+                        onPress={onPress}
+                    />
+                    <Text
+                        style={
+                            checked
+                                ? [
+                                      styles.item_label,
+                                      {color: PaperColor.RadioBtnBlockText},
+                                  ]
+                                : [
+                                      styles.item_label,
+                                      {
+                                          color: PaperColor.RadioBtnBlockTextUnchecked,
+                                      },
+                                  ]
+                        }>
+                        {label}
+                    </Text>
+                </View>
+            </TouchableRipple>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    item: {
+        borderRadius: 8,
+        overflow: 'hidden',
+    },
+    item_root: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingLeft: 8,
+    },
+    item_label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        height: 48,
+        textAlignVertical: 'center',
+    },
+});
+
+export default RadioBtnBlock;
--- src/screens/Language.tsx
+++ src/screens/Language.tsx
@@ -1,29 +1,38 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, {useContext} from 'react';
 import {useTranslation} from 'react-i18next';
-import {View, Text} from 'react-native';
-import {Button} from 'react-native-paper';
+import {View, StyleSheet} from 'react-native';
+import RadioBtnBlock from '../components/RadioBtnBlock';
 import {SettingsContext} from '../util/SettingsManager';
 
 const Language: React.FC = () => {
-    const {t, i18n} = useTranslation();
+    const {i18n} = useTranslation();
     const {handleI18nScheme} = useContext(SettingsContext);
 
     return (
         <View>
-            <Text>{t('Language.Current lang')}</Text>
-            <Button
-                onPress={() => {
-                    if (i18n.language === 'en') {
-                        handleI18nScheme('zh-Hans');
-                    } else {
-                        handleI18nScheme('en');
-                    }
-                }}>
-                {t('Language.Switch')}
-            </Button>
+            <RadioBtnBlock
+                label="English"
+                checked={i18n.language === 'en'}
+                onPress={() => handleI18nScheme('en')}
+                style={styles.item}
+            />
+            <RadioBtnBlock
+                label="简体中文"
+                checked={i18n.language === 'zh-Hans'}
+                onPress={() => handleI18nScheme('zh-Hans')}
+                style={styles.item}
+            />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    root: {},
+    item: {
+        marginLeft: 16,
+        marginRight: 16,
+        marginBottom: 8,
+    },
+});
 
 export default Language;
