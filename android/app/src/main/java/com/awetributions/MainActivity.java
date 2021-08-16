1b488c1bd45ec88bcdc3f02d545054fc29894d25
shensven <shensven2016@gmail.com>
2021-08-16 20:22:15 +0800

    chore: gesture handler library set up on Android

--- android/app/src/main/java/com/awetributions/MainActivity.java
+++ android/app/src/main/java/com/awetributions/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import android.content.res.Configuration;
 import com.zoontek.rnbootsplash.RNBootSplash;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 
 public class MainActivity extends ReactActivity {
@@ -15,6 +18,16 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Awetributions";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 
   /**
