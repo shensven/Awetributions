e24affa9360d77bfa1a646688bbc2efb25b84ef0
shensven <shensven2016@gmail.com>
2021-08-17 23:48:30 +0800

    feat: add axios interceptor

---
+++ src/util/appAxios.ts
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import host from './host';
+
+const appAxios = axios.create({
+    baseURL: host.url,
+    timeout: 5000,
+});
+
+appAxios.interceptors.request.use(
+    config => {
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    },
+);
+
+appAxios.interceptors.response.use(
+    config => {
+        return config;
+    },
+    error => {
+        return Promise.reject(error);
+    },
+);
+
+export default appAxios;
---
+++ src/util/host.ts
@@ -0,0 +1,3 @@
+export default {
+    url: 'https://api.github.com/',
+};
