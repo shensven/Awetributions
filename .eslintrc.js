417030853192165200a49870065ca081107a0364
shensven <shensven2016@gmail.com>
2021-08-26 01:26:57 +0800

    chore: eslint config update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,5 +4,6 @@ module.exports = {
     rules: {
         '@typescript-eslint/no-unused-vars': 'warn',
         'react-hooks/exhaustive-deps': 'warn',
+        'react-native/no-inline-styles': 'off',
     },
 };
