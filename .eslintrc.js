df50b23e8b76a6f89f8cc01436a0312ae50d362a
shensven <shensven2016@gmail.com>
2021-09-03 21:17:22 +0800

    chore: ESLint rule update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,6 @@ module.exports = {
         '@typescript-eslint/no-unused-vars': 'warn',
         'react-hooks/exhaustive-deps': 'warn',
         'react-native/no-inline-styles': 'off',
+        'react-native/no-unused-styles': 1,
     },
 };
